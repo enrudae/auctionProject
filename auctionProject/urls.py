ac7d02c8b14ece160129c3972366143513822fa1
dmitry <malyutin994@gmail.com>
2023-06-06 15:34:08 +0500

    fixed the work with pictures

--- auctionProject/urls.py
+++ auctionProject/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.urls import path, include, re_path
 from auction.views import ActiveLotsAPIList, LotAPIUpdate, LotAPIDestroy, NewBetInLotUpdate, BuyersLotsAPIList
@@ -17,4 +19,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
